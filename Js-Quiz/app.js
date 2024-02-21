b0ee91d85b8ea714fcb293248e7bc951727bdab3
Abhay <abhaypatil8055@gmail.com>
2024-02-21 11:27:20 +0530

    update
    update

--- Js-Quiz/app.js
+++ Js-Quiz/app.js
@@ -1,57 +1,57 @@
-var NumberOfQuestions = 5;
+var numberOfQuestions = 6;
 var countAnswer = 0;
-var options = [];
+var option = [];
 var currentQuestionIndex = 0; // Initialize the index
-var selectOption1 = () => {   // onclick Event
+
+var selectOption1 = () => {
     resetOption();
-    if (options[0].IsCorrect == true) {
-        document.querySelector("#firstOption").style.backgroundColor = "greenyellow";
+    if (option[0].IsCorrect == true) {
+        document.querySelector("#firstOption").style.backgroundColor = "Greenyellow";
         countAnswer++;
-    }
-    else {
+    } else {
         document.querySelector("#firstOption").style.backgroundColor = "red";
     }
     setTimeout(() => {
-        generateQuestion();  // automatically move to next question after 1 sec delay
+        generateQuestion();
     }, 1000);
 }
+
 var selectOption2 = () => {
     resetOption();
-    if (options[1].IsCorrect == true) {
-        document.querySelector("#secondOption").style.backgroundColor = "greenyellow";
+    if (option[1].IsCorrect == true) {
+        document.querySelector("#secondOption").style.backgroundColor = "Greenyellow";
         countAnswer++;
-    }
-    else {
+    } else {
         document.querySelector("#secondOption").style.backgroundColor = "red";
     }
     setTimeout(() => {
-        generateQuestion();  // automatically move to next question after 1 sec delay
+        generateQuestion();
     }, 1000);
 }
+
 var selectOption3 = () => {
     resetOption();
-    if (options[2].IsCorrect == true) {
-        document.querySelector("#thirdOption").style.backgroundColor = "greenyellow";
+    if (option[2].IsCorrect == true) {
+        document.querySelector("#thirdOption").style.backgroundColor = "Greenyellow";
         countAnswer++;
-    }
-    else {
+    } else {
         document.querySelector("#thirdOption").style.backgroundColor = "red";
     }
     setTimeout(() => {
-        generateQuestion(); // automatically move to next question after 1 sec delay
+        generateQuestion();
     }, 1000);
 }
+
 var selectOption4 = () => {
     resetOption();
-    if (options[3].IsCorrect == true) {
-        document.querySelector("#forthOption").style.backgroundColor = "greenyellow";
+    if (option[3].IsCorrect == true) {
+        document.querySelector("#fourthOption").style.backgroundColor = "Greenyellow";
         countAnswer++;
-    }
-    else {
-        document.querySelector("#forthOption").style.backgroundColor = "red";
+    } else {
+        document.querySelector("#fourthOption").style.backgroundColor = "red";
     }
     setTimeout(() => {
-        generateQuestion();   // automatically move to next question after 1 sec delay
+        generateQuestion();
     }, 1000);
 }
 
@@ -59,99 +59,85 @@ var resetOption = () => {    // clear the value
     document.querySelector("#firstOption").style.backgroundColor = "";
     document.querySelector("#secondOption").style.backgroundColor = "";
     document.querySelector("#thirdOption").style.backgroundColor = "";
-    document.querySelector("#forthOption").style.backgroundColor = "";
+    document.querySelector("#fourthOption").style.backgroundColor = "";
 }
 
 var generateQuestion = () => {
-    options = [];
-    var questionArray = [  // array of questions
-        "HTML Stands for?", "CSS stands for?", "JSON stands for?", "Max value of heading tag is?", "onclick is a?"
-    ];
-
-    // Check if there are more questions
+    option = [];
+    var questionArray = ["What does HTML stand for?", "What is the correct HTML element for the largest heading?", "What does CSS stand for?", "Which property is used to change the background color?", "What is JavaScript?", "Which of the following is not a data type in JavaScript?"];
     if (currentQuestionIndex < questionArray.length) {
         var randomQuestion = questionArray[currentQuestionIndex];
         document.querySelector(".question span").innerHTML = randomQuestion;
 
-        // Update options based on the question
-        if (randomQuestion === "HTML Stands for?") {
-            options = [
-                { value: "A: Hyper Text Markup Langauge", IsCorrect: true },
-                { value: "B: Hyper Text Makeup Langauge", IsCorrect: false },
-                { value: "C: Hyper Text My Langauge", IsCorrect: false },
-                { value: "D: High Text Markup Langauge", IsCorrect: false }
-            ];
-
-        }
-        if (randomQuestion === "CSS stands for?") {
-            options = [
-                { value: "A: Case Style Sheet", IsCorrect: false },
-                { value: "B: Cascading Style Sheet", IsCorrect: true },
-                { value: "C: Cascading Style Set", IsCorrect: false },
-                { value: "D: Cascading Set Style", IsCorrect: false }
+        if (randomQuestion === "What does HTML stand for?") {
+            option = [
+                { value: "A : Hyper Text Markup Langauge", IsCorrect: true },
+                { value: "B : Hyper Text Makeup Langauge", IsCorrect: false },
+                { value: "C : Hyper Text My Langauge", IsCorrect: false },
+                { value: "D : High Text Markup Langauge", IsCorrect: false }
             ];
         }
-        if (randomQuestion === "JSON stands for?") {
-            options = [
-                { value: "A: Java Script Object Notation", IsCorrect: true },
-                { value: "B: Java Script Over Notation", IsCorrect: false },
-                { value: "C: Java Script Object Note", IsCorrect: false },
-                { value: "D: Java Search Object Notation", IsCorrect: false }
+        if (randomQuestion === "What is the correct HTML element for the largest heading?") {
+            option = [
+                { value: "A : <h1>", IsCorrect: false },
+                { value: "B : <headings>", IsCorrect: false },
+                { value: "C : <h6>", IsCorrect: true },
+                { value: "D : <large>", IsCorrect: false }
             ];
         }
-        if (randomQuestion === "Max value of heading tag is?") {
-            options = [
-                { value: "A: H5", IsCorrect: false },
-                { value: "B: H6", IsCorrect: true },
-                { value: "C: H8", IsCorrect: false },
-                { value: "D: H9", IsCorrect: false },
+        if (randomQuestion === "What does CSS stand for?") {
+            option = [
+                { value: "A : Case Style Sheet", IsCorrect: false },
+                { value: "B : Cascading Style Sheet", IsCorrect: true },
+                { value: "C : Cascading Style Set", IsCorrect: false },
+                { value: "D : Cascading Set Style", IsCorrect: false }
             ];
         }
-        if (randomQuestion === "onclick is a?") {
-            options = [
-                { value: "A: String", IsCorrect: false },
-                { value: "B: Tag", IsCorrect: false },
-                { value: "C: Array", IsCorrect: false },
-                { value: "D: Method", IsCorrect: true }
+        if (randomQuestion === "Which property is used to change the background color?") {
+            option = [
+                { value: "A : color", IsCorrect: false },
+                { value: "B : background-color", IsCorrect: true },
+                { value: "C : bgcolor", IsCorrect: false },
+                { value: "D : back-color", IsCorrect: false }
             ];
         }
-        
+        if (randomQuestion === "What is JavaScript?") {
+            option = [
+                { value: "A : A programming language", IsCorrect: true },
+                { value: "B : A markup language", IsCorrect: false },
+                { value: "C : A stylesheet language", IsCorrect: false },
+                { value: "D : A machine Learning language", IsCorrect: false }
+            ];
+        }
+        if (randomQuestion === "Which of the following is not a data type in JavaScript?") {
+            option = [
+                { value: "A : Boolean", IsCorrect: false },
+                { value: "B : String", IsCorrect: false },
+                { value: "C : Integer", IsCorrect: true },
+                { value: "D : Null", IsCorrect: false }
+            ]
+        }
         // Update option values
-        document.querySelector("#firstOption").value = options[0].value;
-        document.querySelector("#secondOption").value = options[1].value;
-        document.querySelector("#thirdOption").value = options[2].value;
-        document.querySelector("#forthOption").value = options[3].value;
-
+        document.querySelector("#firstOption").value = option[0].value;
+        document.querySelector("#secondOption").value = option[1].value;
+        document.querySelector("#thirdOption").value = option[2].value;
+        document.querySelector("#fourthOption").value = option[3].value;
 
         resetOption();
 
-        // Increment the current question index for the next question
         currentQuestionIndex++;
     } else {
         // Display a message when all questions are answered
-        var correctAnswer = countAnswer;
-        var percentage = correctAnswer / NumberOfQuestions * 100;
-        var grade;
-        if (percentage > 50) {
-            grade = "Conguratulations !! You are on the right Path";
-        } else {
-            grade = "Oops..You need more parctice";
-        }
-        document.querySelector("span").innerHTML = `You Gave ${countAnswer} Correct Answers out of 5`;
-        document.querySelector(".footer div").style.color = "Green";
-        document.querySelector(".footer div").style.fontSize = "2rem";
-        document.querySelector(".footer div").innerHTML = `Got Percentage : ${percentage} %`;
-        document.querySelector(".footer span").innerHTML = `${grade}`;
-        document.querySelector(".retake").style.display = "Block";
-        document.querySelector(".main").style.backgroundColor = "white";
-        document.querySelector("span").style.color = "red";
-        document.querySelector(".answer").style.display = "none";
-
+        document.querySelector(".question span").innerHTML = `You Gave ${countAnswer} Correct Answers out of 6`;
+        document.querySelector(".answers").style.display = "none";
+        document.querySelector("#retake").style.display = "block";
     }
-
 }
-generateQuestion();
+
 var retakeQuiz = () => {
     location.reload();
-
 }
+
+$(document).ready(() => {
+    generateQuestion();
+});
