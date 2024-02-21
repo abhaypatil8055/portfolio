fd1914697d75f24a2e71f4c0c73a5dd4a9c34e45
Abhay <abhaypatil8055@gmail.com>
2024-02-21 19:00:15 +0530

    asdfghj
    asdfghj

--- ShoppersStop/app.js
+++ ShoppersStop/app.js
@@ -2,25 +2,25 @@ var getMens = () => {
     location.href = "file:///E:/UIFullStackTraining/VirtualServer/public/GitHub/portfolio/ShoppersStop/mensFashion/mensFashion.html";
 }
 var getWomen = () => {
-    location.href = "file:///E:/UIFullStackTraining/VirtualServer/public/JS-Mini-Project/AmazonClone/womensFashion/womensFashion.html";
+    location.href = "file:///E:/UIFullStackTraining/VirtualServer/public/GitHub/portfolio/ShoppersStop/womensFashion/womensFashion.html";
     // window.open is used to open a new page on the new window
 }
 var getBooks = () => {
-    location.href = "file:///E:/UIFullStackTraining/VirtualServer/public/JS-Mini-Project/AmazonClone/shopsBook/ShopBooks.html";
+    location.href = "file:///E:/UIFullStackTraining/VirtualServer/public/GitHub/portfolio/ShoppersStop/shopsBook/ShopBooks.html";
     // location.href is used to open a new page on the same window with back button
 }
 var getPets = () => {
-    location.href = "file:///E:/UIFullStackTraining/VirtualServer/public/JS-Mini-Project/AmazonClone/pets/pets.html";
+    location.href = "file:///E:/UIFullStackTraining/VirtualServer/public/GitHub/portfolio/ShoppersStop/pets/pets.html";
 }
 var getHealthBeauty = () => {
-    location.href = "file:///E:/UIFullStackTraining/VirtualServer/public/JS-Mini-Project/AmazonClone/healthAndBeauty/healthAndBeauty.html";
+    location.href = "file:///E:/UIFullStackTraining/VirtualServer/public/GitHub/portfolio/ShoppersStop/healthAndBeauty/healthAndBeauty.html";
 }
 var getKitchenFavorites = () => {
-    location.href = "file:///E:/UIFullStackTraining/VirtualServer/public/JS-Mini-Project/AmazonClone/kitchenFavorites/kitchenFavorites.html";
+    location.href = "file:///E:/UIFullStackTraining/VirtualServer/public/GitHub/portfolio/ShoppersStop/kitchenFavorites/kitchenFavorites.html";
 }
 var getDiy = () => {
-    location.href = "file:///E:/UIFullStackTraining/VirtualServer/public/JS-Mini-Project/AmazonClone/diy/diy.html";
+    location.href = "file:///E:/UIFullStackTraining/VirtualServer/public/GitHub/portfolio/ShoppersStop/diy/diy.html";
 }
 var getElectronics = () => {
-    location.href = "file:///E:/UIFullStackTraining/VirtualServer/public/JS-Mini-Project/AmazonClone/electronics/electronics.html";
+    location.href = "file:///E:/UIFullStackTraining/VirtualServer/public/GitHub/portfolio/ShoppersStop/electronics/electronics.html";
 }
